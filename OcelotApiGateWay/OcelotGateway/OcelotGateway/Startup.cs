1d4f92eb0e1c6407477d182006da57c046bc2ed0
newmancroos <newmancroos@gmail.com>
2021-11-17 17:44:19 -0500

    added Authentication and commented it

--- OcelotApiGateWay/OcelotGateway/OcelotGateway/Startup.cs
+++ OcelotApiGateWay/OcelotGateway/OcelotGateway/Startup.cs
@@ -13,6 +13,25 @@ namespace OcelotGateway
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOcelot();
+
+            //var secret = "securt";
+            //var key = Encoding.ASCII.GetBytes(secret);
+            //services.AddAuthentication(options =>
+            //{
+            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //}).AddJwtBearer(options =>
+            //{
+            //    options.RequireHttpsMetadata = false;
+            //    options.SaveToken = true;
+            //    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+            //    {
+            //        IssuerSigningKey = new SymmetricSecurityKey(key),
+            //        ValidateIssuerSigningKey = true,
+            //        ValidateIssuer = false,
+            //        ValidateAudience = false,
+            //    };
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,7 +44,7 @@ namespace OcelotGateway
 
             
             app.UseRouting();
-
+            //app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
